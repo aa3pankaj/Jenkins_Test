75f619b3ccfb7ad614afc63f864075788a8002bd
pkz <admin@my.com>
2018-12-10 02:06:20 +0530

    pkz

--- Test/src/com/JenkinsTest.java
+++ Test/src/com/JenkinsTest.java
@@ -3,7 +3,7 @@ package com;
 public class JenkinsTest {
 
 	public static void main(String[] args) {
-		System.out.println("I am Pankaj");
+		System.out.println("I am PKZ");
 		String[] str= {"pankaj","singh","pop","push"};
 		
 		for(String s:str) {
