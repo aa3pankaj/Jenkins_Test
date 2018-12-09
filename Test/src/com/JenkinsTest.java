4d4a9a68618b8280facef8a8fb3fbca9f6adcb6d
pkz <admin@my.com>
2018-12-10 01:13:54 +0530

    first commit

---
+++ Test/src/com/JenkinsTest.java
@@ -0,0 +1,17 @@
+package com;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		System.out.println("I am Pankaj");
+		String[] str= {"pankaj","singh","pop","push"};
+		
+		for(String s:str) {
+			
+			System.out.println(s);
+			
+		}
+
+	}
+
+}
